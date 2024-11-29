220355edce4be27e25f3741005472c7d8ed4d9bf
Carsen Greenwood <362570749@GoTVDSB.ca>
2024-11-29 09:39:43 -0500

    Working

--- Topic 1-5 Summative Animation/Game1.cs
+++ Topic 1-5 Summative Animation/Game1.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -19,9 +20,11 @@ namespace Topic_1_5_Summative_Animation
 
         Texture2D blackCarTexture;
         Rectangle blackCarRect;
+        Vector2 blackCarSpeed;
 
         Texture2D redCarTexture;
         Rectangle redCarRect;
+        Vector2 redCarSpeed;
 
         Texture2D highwayTexture;
 
@@ -33,6 +36,9 @@ namespace Topic_1_5_Summative_Animation
 
         MouseState mouseState;
 
+        SoundEffect introMusic;
+        SoundEffectInstance introInstance;
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -43,6 +49,12 @@ namespace Topic_1_5_Summative_Animation
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            blackCarRect = new Rectangle(410, 500, 100, 150);
+            blackCarSpeed = new Vector2(-2, -2);
+
+            redCarRect = new Rectangle(540, 450, 90, 150);
+            redCarSpeed = new Vector2(-3, -3);
+
             window = new Rectangle(0, 0, 800, 600);
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.PreferredBackBufferHeight = window.Height;
@@ -62,6 +74,8 @@ namespace Topic_1_5_Summative_Animation
             blackCarTexture = Content.Load<Texture2D>("blackCar");
             redCarTexture = Content.Load<Texture2D>("redCar");
             introFont = Content.Load<SpriteFont>("IntroFont");
+            introMusic = Content.Load<SoundEffect>("IntroMusic");
+            introInstance = introMusic.CreateInstance();
         }
 
         protected override void Update(GameTime gameTime)
@@ -71,12 +85,16 @@ namespace Topic_1_5_Summative_Animation
                 Exit();
             if (screen == screen.Intro)
             {
-                if (mouseState.LeftButton == ButtonState.Pressed)
+                if (mouseState.LeftButton == ButtonState.Pressed || introInstance.State == SoundState.Stopped)
                     screen = screen.Road;
             }
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
+            else if (screen == screen.Road)
+            {
+                // TODO: Add your update logic here
+                blackCarRect.Y += (int)blackCarSpeed.Y;
+                redCarRect.Y += (int)redCarSpeed.Y;
+            }
+                base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
@@ -89,11 +107,15 @@ namespace Topic_1_5_Summative_Animation
             if (screen == screen.Intro)
             {
                 _spriteBatch.Draw(trafficTexture, window, Color.Gray);
-                _spriteBatch.DrawString(introFont, "TRAFFIC JAM", new Vector2(265, 205), Color.Blue);
+                _spriteBatch.DrawString(introFont, "TRAFFIC JAM", new Vector2(265, 285), Color.Red);
+                _spriteBatch.DrawString(introFont, "CLICK   TO   PROCEED", new Vector2(195, 535), Color.Blue);
+                introInstance.Play();
             }
             else if (screen == screen.Road)
             {
                 _spriteBatch.Draw(highwayTexture, window, Color.White);
+                _spriteBatch.Draw(blackCarTexture, blackCarRect, Color.White);
+                _spriteBatch.Draw(redCarTexture, redCarRect, Color.White);
             }
 
             _spriteBatch.End();
